2aed9498f32c43881304cc93bdeb08be1c825a8d
David Fischer <david.fischer.ch@gmail.com>
2018-06-17 13:59:27 +0200

    Replace metadata by indentities

--- reco.py
+++ reco.py
@@ -7,6 +7,7 @@ import bz2, os
 from urllib.request import urlopen
 
 import cv2, numpy as np
+from pytoolbox import filesystem
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
@@ -26,18 +27,18 @@ def main():
     # Initialize
     alignment = load_alignment_lib()
     model = load_model()
-    metadata = load_metadata(os.path.join(face_path, 'images'))
-    embedded = np.zeros((metadata.shape[0], 128))
-    for i, m in enumerate(metadata):
-        image = load_image(m.image_path())
+    identities = load_identities(os.path.join(face_path, 'images'))
+    embedded = np.zeros((identities.shape[0], 128))
+    for counter, identity in enumerate(identities):
+        image = load_image(identity.path)
         image = align_image(alignment, image)
         # scale RGB values to interval [0,1]
         image = (image / 255).astype(np.float32)
         # obtain embedding vector for image
-        embedded[i] = model.predict(np.expand_dims(image, axis=0))[0]
+        embedded[counter] = model.predict(np.expand_dims(image, axis=0))[0]
 
     # Train
-    targets = np.array([m.name for m in metadata])
+    targets = np.array([i.name for i in identities])
     encoder = LabelEncoder()
     encoder.fit(targets)
 
@@ -45,8 +46,8 @@ def main():
     y = encoder.transform(targets)
 
     # Train with only 1/4 of the dataset, then test with 3/4
-    test_idx = np.arange(metadata.shape[0]) % 4 != 0
-    train_idx = np.arange(metadata.shape[0]) % 4 == 0
+    test_idx = np.arange(identities.shape[0]) % 4 != 0
+    train_idx = np.arange(identities.shape[0]) % 4 == 0
 
     X_train = embedded[train_idx]
     X_test = embedded[test_idx]
@@ -66,20 +67,16 @@ def main():
     print(f'KNN accuracy = {acc_knn}, SVM accuracy = {acc_svc}')
 
 
-class IdentityMetadata(object):
+class Identity(object):
 
-    __slots__ = ('base', 'name', 'file')
+    __slots__ = ('name', 'path')
 
-    def __init__(self, base, name, file):
-        self.base = base  # dataset base directory
-        self.name = name  # identity name
-        self.file = file  # image file name
+    def __init__(self, name, path):
+        self.name = name  # person name
+        self.path = path  # image path
 
     def __repr__(self):
-        return self.image_path()
-
-    def image_path(self):
-        return os.path.join(self.base, self.name, self.file)
+        return f"Identity(name='{self.name}', path='{self.path}')"
 
 
 def align_image(alignment, image):
@@ -101,20 +98,17 @@ def load_alignment_lib(filename=ALIGNMENT_DATA_FILENAME):
     return AlignDlib(filename)
 
 
+def load_identities(path, patterns=('*.jpg', '*.jpeg'), **kwargs):
+    identities = []
+    for name in os.listdir(path):
+        for filename in filesystem.find_recursive(os.path.join(path, name), patterns, **kwargs):
+            identities.append(Identity(name, filename))
+    return np.array(identities)
+
+
 def load_image(path):
     """Reverse channels because OpenCV loads images in BGR mode."""
     return cv2.imread(path, 1)[..., ::-1]
-
-
-def load_metadata(path):
-    metadata = []
-    for i in os.listdir(path):
-        for f in os.listdir(os.path.join(path, i)):
-            # Check file extension. Allow only jpg/jpeg' files.
-            ext = os.path.splitext(f)[1]
-            if ext == '.jpg' or ext == '.jpeg':
-                metadata.append(IdentityMetadata(path, i, f))
-    return np.array(metadata)
 
 
 def load_model():
